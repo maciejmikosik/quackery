2e722ed678ecff964c0f50fa7aa2a2d1c3a5b0ae
Maciej Mikosik <maciej@mikosik.com>
2014-12-03 06:31:00 +0100

    created TestBuilder

---
+++ main/java/org/testanza/TestBuilder.java
@@ -0,0 +1,33 @@
+package org.testanza;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+// TODO untested
+public class TestBuilder {
+  private final TestSuite suite;
+
+  public TestBuilder(String name) {
+    this.suite = new TestSuite(name);
+  }
+
+  public <T> void testThat(T item, Tester<T> tester) {
+    suite.addTest(tester.test(item));
+  }
+
+  public <T> void testThatAll(Iterable<T> items, Tester<T> tester) {
+    for (T item : items) {
+      suite.addTest(tester.test(item));
+    }
+  }
+
+  public <T> void testThatAll(T[] items, Tester<T> tester) {
+    for (T item : items) {
+      suite.addTest(tester.test(item));
+    }
+  }
+
+  public Test build() {
+    return suite;
+  }
+}
