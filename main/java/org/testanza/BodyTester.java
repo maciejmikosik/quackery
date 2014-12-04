2b6167dfacf32fe28beab6c43d9bcd56d73e6650
Maciej Mikosik <maciej@mikosik.com>
2014-12-04 16:15:08 +0100

    made BodyTester create unique names for tests

--- main/java/org/testanza/BodyTester.java
+++ main/java/org/testanza/BodyTester.java
@@ -1,15 +1,20 @@
 package org.testanza;
 
+import static java.lang.System.identityHashCode;
 import junit.framework.Test;
 import junit.framework.TestCase;
 
 public abstract class BodyTester<T> implements Tester<T> {
   public Test test(final T item) {
-    return new TestCase(name(item)) {
+    return new TestCase(uniqueName(item)) {
       protected void runTest() throws Throwable {
         body(item);
       }
     };
+  }
+
+  private String uniqueName(final T item) {
+    return name(item) + " #" + identityHashCode(item);
   }
 
   protected abstract String name(T item);
