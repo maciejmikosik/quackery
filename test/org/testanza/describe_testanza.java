cee7ecd40a222422dbb2000cd114b49081969293
Maciej Mikosik <maciej@mikosik.com>
2014-12-05 00:48:06 +0100

    gathered all failures in testanza self test

--- test/org/testanza/describe_testanza.java
+++ test/org/testanza/describe_testanza.java
@@ -1,16 +1,25 @@
 package org.testanza;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.testanza.testers.describe_TestMembers_hasModifier;
 
 public class describe_testanza {
+  private static List<Throwable> failures = new ArrayList<Throwable>();
+
   public static void main(String[] args) throws Throwable {
     runTestsIn(describe_TestMembers_hasModifier.class);
     runTestsIn(describe_BodyTest.class);
     runTestsIn(describe_TestBuilder.class);
-    System.out.println("successful");
+    System.out.println(failures.size() + " failures");
+    for (Throwable failure : failures) {
+      System.out.println();
+      failure.printStackTrace(System.out);
+    }
   }
 
   public static void verify(boolean condition) {
@@ -22,7 +31,11 @@ public class describe_testanza {
   private static void runTestsIn(Class<?> type) throws Throwable {
     for (Method method : type.getDeclaredMethods()) {
       if (isPublic(method) && isStatic(method) && hasNoParameters(method)) {
-        method.invoke(null);
+        try {
+          method.invoke(null);
+        } catch (InvocationTargetException e) {
+          failures.add(e.getTargetException());
+        }
       }
     }
   }
