1e59d6723066861021d10a42520669452ce7a92b
Maciej Mikosik <maciej@mikosik.com>
2014-12-11 12:34:41 +0100

    added new api classes to Build

--- main/java/org/testanza/Build.java
+++ main/java/org/testanza/Build.java
@@ -1,8 +1,11 @@
 package org.testanza;
 
+import static java.util.Arrays.asList;
+
 import org.testanza.testers.TestersForClasses;
 
 class Build {
+  Object core = asList(Test.class, Case.class, Suite.class);
   Class<?> helpers = CaseTester.class;
   Class<?> testBuilder = TestBuilder.class;
   Class<?> testersForClasses = TestersForClasses.class;
