127e58910eab31e2547eedf26d9bf699a7187a72
Maciej Mikosik <maciej@mikosik.com>
2014-12-11 13:53:37 +0100

    added Junit to Build

--- main/java/org/testanza/Build.java
+++ main/java/org/testanza/Build.java
@@ -6,4 +6,5 @@ class Build {
   Object core = asList(Test.class, Case.class, Suite.class, Tester.class);
   Object helpers = asList(CaseTester.class, SuiteTester.class);
   Object testers = asList(Testers.class);
+  Object integration = asList(Junit.class);
 }
