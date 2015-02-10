a3f1c6ac140ce5eb71715a246722037d9936330f
Maciej Mikosik <maciej@mikosik.com>
2015-02-10 10:50:51 +0100

    moved AnnotationElement utilities to common.Classes

--- main/java/org/testanza/Testers.java
+++ main/java/org/testanza/Testers.java
@@ -3,10 +3,13 @@ package org.testanza;
 import static java.util.Arrays.asList;
 import static java.util.Objects.deepEquals;
 import static org.testanza.TestanzaException.check;
+import static org.testanza.common.Classes.fullName;
+import static org.testanza.common.Classes.kind;
+import static org.testanza.common.Classes.modifiers;
+import static org.testanza.common.Classes.simpleName;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
 import java.util.List;
 
@@ -85,40 +88,6 @@ public class Testers {
 
   private static boolean hasModifier(int modifier, AnnotatedElement element) {
     return (modifiers(element) & modifier) != 0;
-  }
-
-  private static int modifiers(AnnotatedElement element) {
-    if (element instanceof Class<?>) {
-      return ((Class<?>) element).getModifiers();
-    } else if (element instanceof Member) {
-      return ((Member) element).getModifiers();
-    } else {
-      throw new RuntimeException("unknown element " + element);
-    }
-  }
-
-  private static String kind(AnnotatedElement element) {
-    return element.getClass().getSimpleName().toLowerCase();
-  }
-
-  private static String simpleName(AnnotatedElement element) {
-    if (element instanceof Class<?>) {
-      return ((Class<?>) element).getSimpleName();
-    } else if (element instanceof Constructor) {
-      return ((Constructor<?>) element).getDeclaringClass().getSimpleName();
-    } else if (element instanceof Member) {
-      return ((Member) element).getName();
-    } else {
-      throw new RuntimeException("unknown element " + element);
-    }
-  }
-
-  private static String fullName(AnnotatedElement element) {
-    if (element instanceof Class<?>) {
-      return element.toString();
-    } else {
-      return kind(element) + " " + element.toString();
-    }
   }
 
   private static String printLines(String prefix, List<?> objects) {
---
+++ main/java/org/testanza/common/Classes.java
@@ -0,0 +1,41 @@
+package org.testanza.common;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Member;
+
+public class Classes {
+  public static int modifiers(AnnotatedElement element) {
+    if (element instanceof Class<?>) {
+      return ((Class<?>) element).getModifiers();
+    } else if (element instanceof Member) {
+      return ((Member) element).getModifiers();
+    } else {
+      throw new RuntimeException("unknown element " + element);
+    }
+  }
+
+  public static String kind(AnnotatedElement element) {
+    return element.getClass().getSimpleName().toLowerCase();
+  }
+
+  public static String simpleName(AnnotatedElement element) {
+    if (element instanceof Class<?>) {
+      return ((Class<?>) element).getSimpleName();
+    } else if (element instanceof Constructor) {
+      return ((Constructor<?>) element).getDeclaringClass().getSimpleName();
+    } else if (element instanceof Member) {
+      return ((Member) element).getName();
+    } else {
+      throw new RuntimeException("unknown element " + element);
+    }
+  }
+
+  public static String fullName(AnnotatedElement element) {
+    if (element instanceof Class<?>) {
+      return element.toString();
+    } else {
+      return kind(element) + " " + element.toString();
+    }
+  }
+}
