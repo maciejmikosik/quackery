7ab5df55eba81c034e7865a4be3fe129ce32f692
Maciej Mikosik <maciej@mikosik.com>
2015-02-10 04:16:58 +0100

    deleted retired SuiteTester

--- main/java/org/testanza/Build.java
+++ main/java/org/testanza/Build.java
@@ -4,7 +4,7 @@ import static java.util.Arrays.asList;
 
 class Build {
   Object core = asList(Test.class, Case.class, Suite.class, Tester.class);
-  Object helpers = asList(TestBuilder.class, SuiteTester.class);
+  Object helpers = asList(TestBuilder.class);
   Object testers = asList(Testers.class);
   Object integration = asList(Junit.class);
 }
