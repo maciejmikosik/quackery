dd2971d0b93f1664f6d9a4e5c9c7367ad353c46c
Maciej Mikosik <maciej@mikosik.com>
2015-03-23 04:19:32 +0100

    refactored contract test adding detecting alien types

--- test/java/org/quackery/describe_Quacks_quacksLike_collection.java
+++ test/java/org/quackery/describe_Quacks_quacksLike_collection.java
@@ -9,39 +9,33 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.TreeSet;
 
 import org.quackery.testing.bug.collect.MutableList;
 
 public class describe_Quacks_quacksLike_collection {
-  private Tester<Class<?>> tester;
-  private List<Class<?>> bugs;
+  private final Tester<Class<?>> tester = quacksLike(Collection.class);
 
   public void accepts_mutable_list() throws Throwable {
-    tester = quacksLike(Collection.class);
     expectSuccess(tester, MutableList.class);
+  }
 
+  public void detects_collection_bugs() throws Throwable {
+    for (Class<?> bug : bugs(Collection.class)) {
+      expectFailure(tester, bug);
+    }
   }
 
   public void accepts_jdk_collections() throws Throwable {
-    tester = quacksLike(Collection.class);
     expectSuccess(tester, ArrayList.class);
     expectSuccess(tester, LinkedList.class);
     expectSuccess(tester, HashSet.class);
     expectSuccess(tester, TreeSet.class);
   }
 
-  public void detects_wrong_type() throws Throwable {
-    tester = quacksLike(Collection.class);
+  public void detects_alien_types() throws Throwable {
     expectFailure(tester, Object.class);
-  }
-
-  public void detects_collection_bugs_in_mutable_list() throws Throwable {
-    tester = quacksLike(Collection.class);
-    bugs = bugs(Collection.class);
-    for (Class<?> bug : bugs) {
-      expectFailure(tester, bug);
-    }
+    expectFailure(tester, String.class);
+    expectFailure(tester, Integer.class);
   }
 }
