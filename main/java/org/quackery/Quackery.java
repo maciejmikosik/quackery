53353a2777440632a625372660b60aa794ad720c
Maciej Mikosik <maciej@mikosik.com>
2015-03-23 07:52:24 +0100

    imported constants in @Quackery

--- main/java/org/quackery/Quackery.java
+++ main/java/org/quackery/Quackery.java
@@ -1,10 +1,11 @@
 package org.quackery;
 
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD })
+@Retention(RUNTIME)
+@Target(METHOD)
 public @interface Quackery {}
