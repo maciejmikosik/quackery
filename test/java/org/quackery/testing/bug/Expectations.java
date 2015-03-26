4a0d3d9cb8efa79369ad9c4efe66aa3bfd7e264f
Maciej Mikosik <maciej@mikosik.com>
2015-03-26 21:08:22 +0100

    moved nested classes out from Expectations

--- test/java/org/quackery/testing/bug/Expectations.java
+++ test/java/org/quackery/testing/bug/Expectations.java
@@ -1,18 +1,11 @@
 package org.quackery.testing.bug;
 
-import static java.util.Collections.unmodifiableList;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.quackery.AssertionException;
-import org.quackery.AssumptionException;
 import org.quackery.Case;
 import org.quackery.Contract;
 import org.quackery.Suite;
 import org.quackery.Test;
+import org.quackery.testing.Problem;
+import org.quackery.testing.Report;
 
 public class Expectations {
   public static void expectSuccess(Contract<Class<?>> contract, Class<?> implementation) {
@@ -50,114 +43,6 @@ public class Expectations {
       return report;
     } else {
       throw new RuntimeException("");
-    }
-  }
-
-  private static class Report {
-    public List<Problem> problems;
-
-    private Report(List<Problem> problems) {
-      this.problems = problems;
-    }
-
-    public Report() {
-      this(unmodifiableList(new ArrayList<Problem>()));
-    }
-
-    public Report add(Problem problem) {
-      List<Problem> newProblems = new ArrayList<>(problems);
-      newProblems.add(problem);
-      return new Report(unmodifiableList(newProblems));
-    }
-
-    public Report merge(Report report) {
-      List<Problem> newProblems = new ArrayList<>(problems);
-      newProblems.addAll(report.problems);
-      return new Report(unmodifiableList(newProblems));
-    }
-
-    public List<Problem> failures() {
-      List<Problem> failures = new ArrayList<>();
-      for (Problem result : problems) {
-        if (result.isFailure()) {
-          failures.add(result);
-        }
-      }
-      return failures;
-    }
-
-    public List<Problem> errors() {
-      List<Problem> errors = new ArrayList<>();
-      for (Problem result : problems) {
-        if (result.isError()) {
-          errors.add(result);
-        }
-      }
-      return errors;
-    }
-
-    public List<Problem> misassumptions() {
-      List<Problem> misassumptions = new ArrayList<>();
-      for (Problem result : problems) {
-        if (result.isError()) {
-          misassumptions.add(result);
-        }
-      }
-      return misassumptions;
-    }
-
-    public String toString() {
-      List<Problem> failures = failures();
-      List<Problem> errors = errors();
-      List<Problem> misassumptions = misassumptions();
-
-      StringBuilder builder = new StringBuilder();
-      builder.append("\nfound ").append(failures.size()).append(" failures:");
-      for (Problem result : failures) {
-        builder.append("\n").append(result.test.name).append("\n");
-      }
-      builder.append("\nfound ").append(errors.size()).append(" errors:");
-      for (Problem result : errors) {
-        builder.append("\n").append(result.test.name).append("\n");
-        builder.append(printStackTrace(result.throwable));
-      }
-      builder.append("\nfound ").append(misassumptions.size()).append(" misassumptions:");
-      for (Problem result : misassumptions) {
-        builder.append("\n").append(result.test.name).append("\n");
-      }
-      return builder.toString();
-    }
-
-    private static String printStackTrace(Throwable throwable) {
-      StringWriter writer = new StringWriter();
-      throwable.printStackTrace(new PrintWriter(writer));
-      return writer.toString();
-    }
-  }
-
-  private static class Problem {
-    public final Case test;
-    public final Throwable throwable;
-
-    public Problem(Case test, Throwable problem) {
-      this.test = test;
-      this.throwable = problem;
-    }
-
-    public boolean isSuccess() {
-      return throwable == null;
-    }
-
-    public boolean isFailure() {
-      return throwable instanceof AssertionException;
-    }
-
-    public boolean isMisassumption() {
-      return throwable instanceof AssumptionException;
-    }
-
-    public boolean isError() {
-      return !isSuccess() && !isFailure() && !isMisassumption();
     }
   }
 }
