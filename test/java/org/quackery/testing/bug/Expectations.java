230d26fac39b0c31261dfc687d6fd96e9224e0dd
Maciej Mikosik <maciej@mikosik.com>
2015-03-26 21:12:46 +0100

    moved runAndCatch to testing.Tests.runQuietly

--- test/java/org/quackery/testing/bug/Expectations.java
+++ test/java/org/quackery/testing/bug/Expectations.java
@@ -1,16 +1,15 @@
 package org.quackery.testing.bug;
 
-import org.quackery.Case;
+import static org.quackery.testing.Tests.runQuietly;
+
 import org.quackery.Contract;
-import org.quackery.Suite;
 import org.quackery.Test;
-import org.quackery.testing.Problem;
 import org.quackery.testing.Report;
 
 public class Expectations {
   public static void expectSuccess(Contract<Class<?>> contract, Class<?> implementation) {
     Test test = contract.test(implementation);
-    Report report = runAndCatch(test);
+    Report report = runQuietly(test);
     boolean expected = report.problems.size() == 0;
     if (!expected) {
       throw new AssertionError("expected success of " + implementation.getName() + report);
@@ -19,30 +18,10 @@ public class Expectations {
 
   public static void expectFailure(Contract<Class<?>> contract, Class<?> implementation) {
     Test test = contract.test(implementation);
-    Report report = runAndCatch(test);
+    Report report = runQuietly(test);
     boolean expected = report.failures().size() > 0 && report.errors().size() == 0;
     if (!expected) {
       throw new AssertionError("expected failure of " + implementation.getName() + report);
-    }
-  }
-
-  private static Report runAndCatch(Test test) {
-    if (test instanceof Case) {
-      Case cas = (Case) test;
-      try {
-        cas.run();
-        return new Report();
-      } catch (Throwable t) {
-        return new Report().add(new Problem(cas, t));
-      }
-    } else if (test instanceof Suite) {
-      Report report = new Report();
-      for (Test subtest : ((Suite) test).tests) {
-        report = report.merge(runAndCatch(subtest));
-      }
-      return report;
-    } else {
-      throw new RuntimeException("");
     }
   }
 }
