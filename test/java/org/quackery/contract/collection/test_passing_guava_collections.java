f9962d2e2da385705d7fc0bbe5a32b13057fe09c
Maciej Mikosik <maciej@mikosik.com>
2015-10-10 20:11:05 +0200

    tested guava collections for immutability

--- test/java/org/quackery/contract/collection/test_passing_guava_collections.java
+++ test/java/org/quackery/contract/collection/test_passing_guava_collections.java
@@ -14,15 +14,17 @@ import com.google.common.collect.ImmutableSet;
 public class test_passing_guava_collections {
   private Contract<Class<?>> contract;
 
-  public void immutable_list_quacks_like_list() {
+  public void immutable_list_quacks_like_immutable_list_with_factory() {
     contract = quacksLike(Collection.class)
         .implementing(List.class)
+        .immutable()
         .withFactory("copyOf");
     assertSuccess(contract.test(ImmutableList.class));
   }
 
-  public void immutable_set_quacks_like_collection() {
+  public void immutable_set_quacks_like_immutable_collection_with_factory() {
     contract = quacksLike(Collection.class)
+        .immutable()
         .withFactory("copyOf");
     assertSuccess(contract.test(ImmutableSet.class));
   }
