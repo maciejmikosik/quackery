73edda0657a83e4d7bd8b4948b0a7425d942ee0c
Maciej Mikosik <maciej@mikosik.com>
2018-08-14 04:00:31 +0200

    handled junit initialization errors by wrapping them in tests

--- main/java/org/quackery/junit/ScanJunitTests.java
+++ main/java/org/quackery/junit/ScanJunitTests.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.runner.Runner;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
-import org.quackery.QuackeryException;
+import org.quackery.Case;
 import org.quackery.Test;
 
 public class ScanJunitTests {
@@ -15,15 +15,41 @@ public class ScanJunitTests {
   }
 
   public static List<Test> causes(InitializationError error) {
-    if (isBecauseNoRunnableMethods(error)) {
-      return new ArrayList<>();
-    } else {
-      throw new QuackeryException(error);
+    boolean hasJunitTestMethods = hasJunitTestMethods(error);
+
+    List<Test> errors = new ArrayList<>();
+    for (final Throwable cause : error.getCauses()) {
+      if (noRunnableMethods(cause)) {
+        continue;
+      } else if (noPublicDefaultConstructor(cause) && !hasJunitTestMethods) {
+        continue;
+      } else {
+        errors.add(new Case(cause.getMessage()) {
+          public void run() throws Throwable {
+            throw cause;
+          }
+        });
+      }
     }
+    return errors;
   }
 
-  private static boolean isBecauseNoRunnableMethods(InitializationError error) {
-    List<Throwable> causes = error.getCauses();
-    return causes.size() == 1 && causes.get(0).getMessage().equals("No runnable methods");
+  private static boolean hasJunitTestMethods(InitializationError error) {
+    for (Throwable cause : error.getCauses()) {
+      if (noRunnableMethods(cause)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean noRunnableMethods(Throwable cause) {
+    return cause.getMessage().equals("No runnable methods");
+  }
+
+  private static boolean noPublicDefaultConstructor(Throwable cause) {
+    String message = cause.getMessage();
+    return message.equals("Test class should have exactly one public constructor")
+        || message.equals("Test class should have exactly one public zero-argument constructor");
   }
 }
--- test/java/org/quackery/junit/TestQuackeryRunnerInitialization.java
+++ test/java/org/quackery/junit/TestQuackeryRunnerInitialization.java
@@ -1,7 +1,10 @@
 package org.quackery.junit;
 
+import static java.lang.reflect.Modifier.PRIVATE;
 import static java.lang.reflect.Modifier.PUBLIC;
 import static java.lang.reflect.Modifier.STATIC;
+import static net.bytebuddy.dynamic.scaffold.subclass.ConstructorStrategy.Default.NO_CONSTRUCTORS;
+import static org.quackery.junit.JunitClassBuilder.defaultJunitMethod;
 import static org.quackery.junit.JunitClassBuilder.defaultQuackeryMethod;
 import static org.quackery.junit.JunitCoreRunner.run;
 import static org.quackery.testing.Assertions.assertEquals;
@@ -91,5 +94,89 @@ public class TestQuackeryRunnerInitialization {
     assertEquals(failure.getDescription().getMethodName(), methodName);
     assertEquals(failure.getException().getClass(), QuackeryException.class);
     assertEquals(failure.getException().getMessage(), "method cannot have parameters");
+  }
+
+  public void reports_missing_default_constructor() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .define(defaultJunitMethod())
+        .load());
+
+    assertEquals(result.getFailureCount(), 1);
+    Failure failure = result.getFailures().get(0);
+    assertEquals(failure.getDescription().getMethodName(),
+        "Test class should have exactly one public constructor");
+    assertEquals(failure.getException().getClass(), Exception.class);
+    assertEquals(failure.getException().getMessage(),
+        "Test class should have exactly one public constructor");
+  }
+
+  public void ignores_missing_default_constructor_if_no_junit_test_methods() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .define(defaultQuackeryMethod()
+            .returning(mockCase(caseName)))
+        .load());
+
+    assertEquals(result.getRunCount(), 1);
+    assertEquals(result.getFailureCount(), 0);
+  }
+
+  public void reports_default_constructor_being_private() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .defineConstructor(new MethodDefinition()
+            .modifiers(PRIVATE)
+            .parameters())
+        .define(defaultJunitMethod())
+        .load());
+
+    assertEquals(result.getFailureCount(), 1);
+    Failure failure = result.getFailures().get(0);
+    assertEquals(failure.getDescription().getMethodName(),
+        "Test class should have exactly one public constructor");
+    assertEquals(failure.getException().getClass(), Exception.class);
+    assertEquals(failure.getException().getMessage(),
+        "Test class should have exactly one public constructor");
+  }
+
+  public void ignores_default_constructor_being_private_if_no_junit_test_methods() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .defineConstructor(new MethodDefinition()
+            .modifiers(PRIVATE)
+            .parameters())
+        .define(defaultQuackeryMethod()
+            .returning(mockCase(caseName)))
+        .load());
+
+    assertEquals(result.getRunCount(), 1);
+    assertEquals(result.getFailureCount(), 0);
+  }
+
+  public void reports_constructor_having_parameters() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .defineConstructor(new MethodDefinition()
+            .modifiers(PUBLIC)
+            .parameters(Object.class))
+        .define(defaultJunitMethod())
+        .load());
+
+    assertEquals(result.getFailureCount(), 1);
+    Failure failure = result.getFailures().get(0);
+    assertEquals(failure.getDescription().getMethodName(),
+        "Test class should have exactly one public zero-argument constructor");
+    assertEquals(failure.getException().getClass(), Exception.class);
+    assertEquals(failure.getException().getMessage(),
+        "Test class should have exactly one public zero-argument constructor");
+  }
+
+  public void ignores_default_constructor_having_parameters_if_no_junit_test_methods() {
+    result = run(new JunitClassBuilder(NO_CONSTRUCTORS)
+        .defineConstructor(new MethodDefinition()
+            .modifiers(PUBLIC)
+            .parameters(Object.class))
+        .define(defaultQuackeryMethod()
+            .returning(mockCase(caseName)))
+        .load());
+
+    assertEquals(result.getRunCount(), 1);
+    assertEquals(result.getFailureCount(), 0);
   }
 }
