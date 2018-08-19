0f919439685e63fc7007404c12b9a89e9bece0d4
Maciej Mikosik <maciej@mikosik.com>
2018-08-20 00:48:41 +0200

    implemented concurrent runner using all available processors

---
+++ test/java/org/quackery/run/TestRunnersRunConcurrent.java
@@ -0,0 +1,71 @@
+package org.quackery.run;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.quackery.Suite.suite;
+import static org.quackery.run.Runners.concurrent;
+import static org.quackery.run.Runners.run;
+import static org.quackery.run.TestingDecorators.decorator_preserves_case_result;
+import static org.quackery.run.TestingDecorators.decorator_preserves_names_and_structure;
+import static org.quackery.run.TestingDecorators.decorator_runs_cases_eagerly;
+import static org.quackery.run.TestingDecorators.decorator_validates_arguments;
+import static org.quackery.testing.Testing.assertTrue;
+import static org.quackery.testing.Testing.fail;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.quackery.Case;
+import org.quackery.QuackeryException;
+import org.quackery.Test;
+import org.quackery.help.Decorator;
+
+public class TestRunnersRunConcurrent {
+  public static void test_runners_run_concurrent() throws Throwable {
+    Decorator decorator = new Decorator() {
+      public Test decorate(Test test) {
+        return run(concurrent(test));
+      }
+    };
+
+    decorator_preserves_names_and_structure(decorator);
+    decorator_preserves_case_result(decorator);
+    decorator_validates_arguments(decorator);
+    decorator_runs_cases_eagerly(decorator);
+
+    runs_concurrently();
+    validates_arguments();
+  }
+
+  private static void runs_concurrently() {
+    assertTrue(Runtime.getRuntime().availableProcessors() >= 3);
+
+    CountDownLatch latch = new CountDownLatch(3);
+    AtomicBoolean failed = new AtomicBoolean(false);
+    Test test = suite("")
+        .add(countDown(latch, failed))
+        .add(countDown(latch, failed))
+        .add(countDown(latch, failed));
+
+    run(concurrent(test));
+
+    assertTrue(!failed.get());
+  }
+
+  private static Case countDown(final CountDownLatch latch, final AtomicBoolean failed) {
+    return new Case("countDown") {
+      public void run() throws InterruptedException {
+        latch.countDown();
+        if (!latch.await(1, SECONDS)) {
+          failed.set(true);
+        }
+      }
+    };
+  }
+
+  private static void validates_arguments() {
+    try {
+      concurrent(null);
+      fail();
+    } catch (QuackeryException e) {}
+  }
+}
